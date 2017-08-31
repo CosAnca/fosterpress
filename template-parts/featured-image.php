701a248e3d6f152db262057bddf15c624f852dc9
Cos <cosmin.anca@yahoo.com>
2017-08-31 12:29:26 +0100

    Remove Interchange reference in featured image template

--- template-parts/featured-image.php
+++ template-parts/featured-image.php
@@ -1,7 +1,5 @@
 <?php
-// If a featured image is set, insert into layout and use Interchange
-// to select the optimal image size per named media query.
+// If a featured image is set, insert into layout.
 if ( has_post_thumbnail( $post->ID ) ) : ?>
-	<header class="featured-hero" role="banner" data-interchange="[<?php echo the_post_thumbnail_url('featured-small'); ?>, small], [<?php echo the_post_thumbnail_url('featured-medium'); ?>, medium], [<?php echo the_post_thumbnail_url('featured-large'); ?>, large], [<?php echo the_post_thumbnail_url('featured-xlarge'); ?>, xlarge]">
-	</header>
+	<header class="featured-hero" role="banner" style="background-image: url('<?php echo the_post_thumbnail_url() ?>')" ></header>
 <?php endif;
